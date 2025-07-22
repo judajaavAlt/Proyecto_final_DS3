1d2aa11a89446cfeb32e9a4d23d5605cad503113
ElDanissito <rojasbarrenechedaniel@gmail.com>
2025-07-22 17:31:12 -0500

    fix: prisma y migraciones robustos en Docker

---
+++ rating-service/backend/prisma/migrations/20250722222305_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Review` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `movieId` INTEGER NOT NULL,
+    `text` VARCHAR(191) NOT NULL,
+    `rating` DOUBLE NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `author` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ rating-service/backend/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+until mysqladmin ping -h"db" -P"3306" --silent; do
+  echo "Esperando a que la base de datos esté lista..."
+  sleep 2
+done
+npx prisma generate
+npx prisma migrate deploy
+npm start 
